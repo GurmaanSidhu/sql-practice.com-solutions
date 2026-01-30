7a233cfaa5eaf7e201d36a2db4825b4002db310d
Gurmaan Sidhu <gurmaansidhu1232006@gmail.com>
2026-01-30 23:17:37 +0530

    initial commit

---
+++ Solutions/medium-3.sql
@@ -0,0 +1,3 @@
+-- Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+
+select patient_id, first_name from patients where first_name like 'S%s' and length(first_name) >= 6;
