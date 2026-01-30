ec9dfd54edcbcf3a9f374fe0c6d95a3d182d1fe6
Gurmaan Sidhu <gurmaansidhu1232006@gmail.com>
2026-01-30 23:27:31 +0530

    initial commit

---
+++ Solutions/medium-16.sql
@@ -0,0 +1,3 @@
+-- Show all of the days of the month (1-31) and how many admission_dates occurred on that day. Sort by the day with most admissions to least admissions.
+
+select day(admission_date), count(patient_id) from admissions group by day(admission_date) order by count(patient_id) desc
---
+++ Solutions/medium-18.sql
@@ -0,0 +1,5 @@
+-- Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria:
+-- 1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19.
+-- 2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.
+
+select patient_id, attending_doctor_id, diagnosis from admissions where (patient_id % 2 = 1 and attending_doctor_id in (1, 5, 19)) or (cast(attending_doctor_id as char) like '%2%' and length(patient_id) = 3)
