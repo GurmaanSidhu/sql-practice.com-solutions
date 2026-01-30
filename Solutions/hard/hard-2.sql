9179b6df1448449d83889be77821ac27fba518a3
Gurmaan Sidhu <gurmaansidhu1232006@gmail.com>
2026-01-31 01:30:26 +0530

    initial commit

--- Solutions/hard/hard-2.sql
+++ Solutions/hard/hard-2.sql
@@ -1,12 +1,9 @@
-Show patient_id, weight, height, isObese from the patients table.
+-- Show patient_id, weight, height, isObese from the patients table.
 
-Display isObese as a boolean 0 or 1.
-
-Obese is defined as weight(kg)/(height(m)2) >= 30.
-
-weight is in units kg.
-
-height is in units cm.
+-- Display isObese as a boolean 0 or 1.
+-- Obese is defined as weight(kg)/(height(m)2) >= 30.
+-- weight is in units kg.
+-- height is in units cm.
 
 select patient_id, weight, height,
 (case when weight / ((height/100.0) * (height/100.0)) >= 30
