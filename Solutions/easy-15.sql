b77065bc6229a063e51a348bae8cc97749cfb961
Gurmaan Sidhu <gurmaansidhu1232006@gmail.com>
2026-01-30 23:16:20 +0530

    initial commit

--- Solutions/easy-15.sql
+++ Solutions/easy-15.sql
@@ -1,3 +1,3 @@
--- Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.
+-- Write a query to find the first_name, last name and birth date of patients who has height greater than 160 and weight greater than 70
 
-select distinct city from patients where province_id = 'NS';
+select first_name, last_name, birth_date from patients where height > 160 and weight > 70;
---
+++ Solutions/medium-1.sql
@@ -0,0 +1,3 @@
+-- Show unique birth years from patients and order them by ascending.
+
+select distinct year(birth_date) from patients order by year(birth_date) asc;
