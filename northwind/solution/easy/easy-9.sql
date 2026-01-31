8b3c85862cc9367a071afdbd61be076d1a697652
Gurmaan Sidhu <gurmaansidhu1232006@gmail.com>
2026-02-01 02:30:21 +0530

    initial commit

---
+++ northwind/solution/easy/easy-9.sql
@@ -0,0 +1,3 @@
+-- Show the average unit price rounded to 2 decimal places, the total units in stock, total discontinued products from the products table.
+
+select round(avg(unit_price), 2) as average_price, sum(units_in_stock) as total_stock, sum(discontinued) as total_discontinued from products    
